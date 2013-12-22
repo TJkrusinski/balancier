23ceb5283bab397a4fccdf7e54eaa5bf8fd27980
TJ Krusinski <tjkrus@gmail.com>
2013-12-21 23:09:13 -0500

    adding and removeing of backends

--- index.js
+++ index.js
@@ -20,20 +20,24 @@ balancier.middleware = function(req, res, proxy) {
  *	Set backends
  *	@method setBackend
  *	@param {Object|Array} backends
+ *	@return {Boolean} optomistic success
  */
 balancier.setBackend = function(backends) {
-	if (backends instanceof Array) return backends.forEach(this.setBackend);
+	if (backends instanceof Array) return !!backends.map(this.setBackend, balancier);
+	if (!backends.port || !backends.host) return false;
 	this._backends.push(backends);
+	return true;
 };
 
 /**
  *	Remove backend
  *	@method remBackend
  *	@param {Object} backend
+ *	@return {Boolean} optomistic success
  */
 balancier.remBackend = function(backend) {
-	if (!backend.port || !backend.host) return;
-	var index;
+	if (!backend.port || !backend.host) return false;
+	var index = -1;
 
 	this._backends.forEach(function(el, i){
 		if (el.port == backend.port &&
@@ -42,7 +46,11 @@ balancier.remBackend = function(backend) {
 		};
 	});
 
+	if (!~index) return false;
+
 	this._backends.splice(index, 1);
+
+	return true;
 };
 
 /**
--- tests/balancier.js
+++ tests/balancier.js
@@ -1,3 +1,72 @@
 'use strict';
 
+var bala = require(__dirname+'/../index.js'),
+	assert = require('chai').assert;
 
+describe('balancier#setBackend()', function(){
+	it('adds a backend to the list', function(){
+		var res = bala.setBackend({
+			port: 3000,
+			host: 'myhost.com'
+		});
+
+		assert.isTrue(res);
+		assert.equal(bala._backends.length, 1);
+	});
+});
+
+describe('balancier#remBackend()', function(){
+	it('removes a backend to the list', function(){
+		var res = bala.remBackend({
+			port: 3000,
+			host: 'myhost.com'
+		});
+		
+		assert.isTrue(res);
+		assert.equal(bala._backends.length, 0);
+	});
+});
+
+describe('balancier#setBackend()', function(){
+	it('adds 2 backends to the list', function(){
+		var res = bala.setBackend([
+			{
+				port: 3000,
+				host: 'myhost.com'
+			},
+			{
+				
+				port: 3000,
+				host: '2.myhost.com'
+			}
+		]);
+
+		assert.isTrue(res);
+		assert.equal(bala._backends.length, 2);
+	});
+});
+
+describe('balancier#remBackend()', function(){
+	it('removes a backend to the list', function(){
+		var res = bala.remBackend({
+			port: 3000,
+			host: 'myhost.com'
+		});
+		
+		assert.isTrue(res);
+		assert.equal(bala._backends.length, 1);
+		assert.equal(bala._backends[0].host, '2.myhost.com');
+	});
+});
+
+describe('balancier#remBackend()', function(){
+	it('removes a backend to the list', function(){
+		var res = bala.remBackend({
+			port: 3000,
+			host: '2.myhost.com'
+		});
+		
+		assert.isTrue(res);
+		assert.equal(bala._backends.length, 0);
+	});
+});
